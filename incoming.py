d7d888adbd4827087d2dde1da7d2d6064f165ffd
Pedro Gimeno <pgimeno@email.fake>
2013-08-01 22:43:46 +0200

    Encode header in MIME for transmission to the email client

--- incoming.py
+++ incoming.py
@@ -3,6 +3,7 @@ import threading
 import email.mime.text
 import email.mime.image
 import email.mime.multipart
+import email.header
 import bminterface
 import re
 
@@ -160,7 +161,7 @@ def makeEmail(dateTime, toAddress, fromAddress, subject, body):
             msg.attach(img)
     msg['To'] = toAddress
     msg['From'] = fromAddress
-    msg['Subject'] = subject
+    msg['Subject'] = email.header.Header(subject, 'UTF-8')
     msg['Date'] = dateTime
     return msg.as_string()
     
